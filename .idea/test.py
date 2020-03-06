56452af08b75b9b233390f48a7b5242b95f8151c
lsg1339 <43155831+lsg1339@users.noreply.github.com>
2020-03-05 18:08:10 -0700

    VirusTotal API installed

--- .idea/test.py
+++ .idea/test.py
@@ -1,13 +1,16 @@
 import pefile
 import datetime
 import time
+import requests
 import os
 
-
-exe_path = input("Please enter your file path : ")
-pe = pefile.PE(exe_path)
-compileTime = datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp)
-packed = False
+directory= input("Please enter your directory: ")
+for filename in os.listdir(directory):
+    if filename.endswith(".exe"):
+        exe_path = filename
+        pe = pefile.PE(exe_path)
+        compileTime = datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp)
+        packed = False
 
 for field in pe.DOS_HEADER.dump():
     print(field)
@@ -19,18 +22,32 @@ for entry in pe.DIRECTORY_ENTRY_IMPORT:
     print('\t' + entry.dll.decode('utf-8'))
 
 for section in pe.sections:
+    ##print(section.Name.decode('utf-8'))
+    ##print("\tVirtual Address: " + hex(section.VirtualAddress))
+    ##print("\tVirtual Size: " + hex(section.Misc_VirtualSize))
+    ##print("\tRaw Size: " + hex(section.SizeOfRawData))
     if int(section.SizeOfRawData) < 1 :
         packed = True
 
 if packed:
-    print("\nFile is likely packed\n")
+    print("Likely packed ")
 else:
-    print("\nFile does not appear to be packed\n")
+    print("File does not appear to be packed")
 
-os.system("pause")
+
+url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+
+params = {'apikey': '1b88f1ccceabaa4b8bb643742c0822628f09d2e03fc58374638b73a4c4d1a1a5'}
+
+files = {'file': ('C:\Windows\System32\calc.exe', open('C:\Windows\System32\calc.exe', 'rb'))}
+
+response = requests.post(url, files=files, params=params)
+
+print(response.json())
 
 ### C:\Windows\System32\calc.exe
 ### C:\Program Files\Core Temp\Core Temp.exe
+### 1b88f1ccceabaa4b8bb643742c0822628f09d2e03fc58374638b73a4c4d1a1a5
 
 ### https://axcheron.github.io/pe-format-manipulation-with-pefile/
 
