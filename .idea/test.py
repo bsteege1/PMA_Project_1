4ff386842ab94ed8d6f76cb8492bb810c0cfaeb0
lsg1339 <43155831+lsg1339@users.noreply.github.com>
2020-03-07 19:38:55 -0700

    Moved functionality to auxiliary.py Formatted console output

--- .idea/test.py
+++ .idea/test.py
@@ -1,55 +1,34 @@
 import pefile
-import datetime
-import time
-import requests
 import os
+import auxiliary
+
 
 directory= input("Please enter your directory: ")
 for filename in os.listdir(directory):
     if filename.endswith(".exe"):
-        print("THIS IS FILENAME: " + filename)
-        file = filename
-        exe_path = directory + "\\" + file
-        print("THIS IS EXE_PATH: " + exe_path)
+        print("\n\n[*] EXE Found: " + filename)
+        fullDirectory = directory + "\\" + filename
+        pe = pefile.PE(fullDirectory)
 
-    pe = pefile.PE(exe_path)
-    compileTime = datetime.datetime.fromtimestamp(pe.FILE_HEADER.TimeDateStamp)
-    packed = False
-    for field in pe.DOS_HEADER.dump():
-        print(field)
+        #Upload to virustotal
+        auxiliary.virusTotal(pe, fullDirectory)
 
-    print("\nCompile Time: " + str(compileTime))
+        #Find compilation time of file
+        auxiliary.timeCompiled(pe)
 
-    print("\n[*] Listing imported DLLs...")
-    for entry in pe.DIRECTORY_ENTRY_IMPORT:
-        print('\t' + entry.dll.decode('utf-8'))
+        #Decide whether file is packed
+        auxiliary.isPacked(pe)
 
-    for section in pe.sections:
-        ##print(section.Name.decode('utf-8'))
-        ##print("\tVirtual Address: " + hex(section.VirtualAddress))
-        ##print("\tVirtual Size: " + hex(section.Misc_VirtualSize))
-        ##print("\tRaw Size: " + hex(section.SizeOfRawData))
-        if int(section.SizeOfRawData) < 1:
-            packed = True
+        #List Imported DLLs
+        auxiliary.importedDlls(pe)
 
-    if packed:
-        print("Likely packed ")
-    else:
-        print("File does not appear to be packed")
+        #Find Strings
+        #auxiliary.findStrings(pe)
 
-    url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-
-    params = {'apikey': '1b88f1ccceabaa4b8bb643742c0822628f09d2e03fc58374638b73a4c4d1a1a5'}
-
-    files = {'file': ('C:\Windows\System32\calc.exe', open('C:\Windows\System32\calc.exe', 'rb'))}
-
-    response = requests.post(url, files=files, params=params)
-
-    print(response.json())
+        for field in pe.DOS_HEADER.dump():
+            print(field)
 else:
-    print("No .exe found")
-
-
+    print("\n[*] Directory contains no untested executables")
 
 
 ### C:\Windows\System32\calc.exe
@@ -57,9 +36,3 @@ else:
 ### 1b88f1ccceabaa4b8bb643742c0822628f09d2e03fc58374638b73a4c4d1a1a5
 
 ### https://axcheron.github.io/pe-format-manipulation-with-pefile/
-
-##print(("TimeDateStamp: "+ hex(pe.FILE_HEADER.TimeDateStamp)), exp.name.decode('utf-8'))
-
-##print((pe.FILE_HEADER.TimeDateStamp))
-
-##print("TimeDateStamp: "+ hex(pe.FILE_HEADER.TimeDateStamp))
